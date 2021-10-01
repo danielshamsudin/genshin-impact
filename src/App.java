d293ab66d9598ba278ca7c925f9cf29d8891a585
unknown <shafiq.azam@alumni.fh-aachen.de>
2021-10-01 18:31:50 +0200

    Main Page runnable

--- src/App.java
+++ src/App.java
@@ -1,6 +1,11 @@
+
+import java.io.IOException;
+
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
@@ -8,25 +13,37 @@ import javafx.stage.Stage;
 
 public class App extends Application{
     
+    private Stage primaryStage;
+
     @Override
-    public void start(Stage primaryStage){
-        Button btn = new Button();
-        btn.setText("Test");
-        btn.setOnAction(new EventHandler<ActionEvent>(){
+    public void start(Stage stage) throws IOException{
+        // Button btn = new Button();
+        // btn.setText("Test");
+        // btn.setOnAction(new EventHandler<ActionEvent>(){
             
-            @Override
-            public void handle(ActionEvent event){
-                System.out.println("Clicked button");
-            }
-        });
+        //     @Override
+        //     public void handle(ActionEvent event){
+        //         System.out.println("Clicked button");
+        //     }
+        // });
 
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
+        // StackPane root = new StackPane();
+        // root.getChildren().add(btn);
 
-        Scene scene = new Scene(root, 300, 250);
-        primaryStage.setTitle("Testing");
+        // Scene scene = new Scene(root, 300, 250);
+        // primaryStage.setTitle("Testing");
+        // primaryStage.setScene(scene);
+        // primaryStage.show();
+        primaryStage = stage;
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("MainPage.fxml"));
+        Parent root = loader.load();
+        MainPageController controller= loader.<MainPageController>getController();
+        controller.setMainApp(this);
+
+        Scene scene = new Scene(root);
         primaryStage.setScene(scene);
         primaryStage.show();
+
     }
 
     public static void main(String[] args) {
---
+++ src/MainPageController.java
@@ -0,0 +1,10 @@
+
+public class MainPageController {
+    App mainApp;
+
+    public void setMainApp(App main){ this.mainApp = main; }
+
+    public void mainButton(){
+        System.out.println("Testing");
+    }
+}
